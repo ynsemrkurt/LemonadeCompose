cb40fcf5902cd2dbf815cb5feec12faa04541018
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-08 12:56:51 +0300

    Create Lemonade App Function

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemonadeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LemonadePreview()
             }
         }
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadePreview() {
+    LemonadeTheme {
+        LemonadeApp(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LemonadeTheme {
-        Greeting("Android")
+fun LemonadeApp(modifier: Modifier = Modifier) {
+    val currentStep = remember { mutableIntStateOf(1) }
+    val (image, text, contentDescription) = fetchImageAndText(currentStep.value)
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(onClick = {
+            currentStep.value++
+            if (currentStep.value == 5) {
+                currentStep.value = 1
+            }
+        }) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = stringResource(contentDescription),
+                modifier = Modifier
+                    .background(Color(0xFFCAEAD3), shape = MaterialTheme.shapes.extraLarge)
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = stringResource(text))
+    }
+}
+
+@Composable
+fun fetchImageAndText(currentStep: Int): Triple<Int, Int, Int> {
+    return when (currentStep) {
+        1 -> Triple(R.drawable.lemon_tree, R.string.text_first, R.string.tree)
+        2 -> Triple(R.drawable.lemon_squeeze, R.string.text_second, R.string.lemon)
+        3 -> Triple(R.drawable.lemon_drink, R.string.text_third, R.string.glass)
+        else -> Triple(R.drawable.lemon_restart, R.string.text_fourth, R.string.empty)
     }
 }
